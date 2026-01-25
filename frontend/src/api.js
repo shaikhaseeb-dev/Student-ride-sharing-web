78d4e631eb1eb4a466536d322c0f27da039596a0
shaikhaseeb-dev <shaikhaseeb004@gmail.com>
2026-01-25 23:13:55 +0530

    Update frontend API base URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://student-ride-sharing-web.onrender.com/api",
 });
 
 // attach token automatically
